2e0b87c3d77ee26d9882d24d2f863f6c9d4bc0c4
Dariusssssssss <sablezzubaya@gmail.com>
2024-10-27 22:47:02 +0300

    completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minValue = 0;
+        this.maxValue = 0;
+        this.middle = 0;
+    }
 
-    setRange(min, max) {
-
+    setRange(minValue, maxValue) {
+        this.minValue = minValue;
+        this.maxValue = maxValue;
     }
 
     guess() {
-
+        this.middle = Math.round((this.minValue + this.maxValue) / 2);
+        return this.middle;
     }
 
     lower() {
+        this.maxValue = this.middle;
 
     }
 
     greater() {
-
+        this.minValue = this.middle;
     }
 }
 
